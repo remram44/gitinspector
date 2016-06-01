ae7d20fa4ec4c9910240c5a90ed8e6a608d23b00
Remi Rampin <remirampin@gmail.com>
2016-06-01 10:51:27 -0400

    Change 'row' to 'line' in HTML output

--- gitinspector/output/blameoutput.py
+++ gitinspector/output/blameoutput.py
@@ -27,7 +27,7 @@ from .. import format, gravatar, terminal
 from ..blame import Blame
 from .outputable import Outputable
 
-BLAME_INFO_TEXT = N_("Below are the number of rows from each author that have survived and are still "
+BLAME_INFO_TEXT = N_("Below are the number of lines from each author that have survived and are still "
                      "intact in the current revision")
 
 class BlameOutput(Outputable):
